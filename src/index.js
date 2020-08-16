448f29407ca05001d36039b0979aa6b85a42ce5c
github username <github email address>
2020-08-16 11:16:29 -0400

    added minor change to comments in files

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import courseReducer from './reducers/courseReducer';
 import App from './App';
-import './App.css';
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// Use applyMiddleware to enable thunk
+
 let store = createStore(courseReducer, composeEnhancers(applyMiddleware(thunk)));
 
-// Wrap entire app in provider to give all components access to the store
 ReactDOM.render(
   <Provider store = { store }>
     <Router>
